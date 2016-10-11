04fa0c3ff4f6c0e4a67d9f8bbf81a87a7c307e75
Dominic Amato <dominic.a.amato@gmail.com>
2016-10-11 16:24:01 -0500

    update error handling of raw code

--- src/main/java/mobi/omegacentauri/raspberryjammod/process/RJMProcessCode.java
+++ src/main/java/mobi/omegacentauri/raspberryjammod/process/RJMProcessCode.java
@@ -15,10 +15,12 @@ import java.util.regex.Pattern;
 
 import mobi.omegacentauri.raspberryjammod.RaspberryJamMod;
 import mobi.omegacentauri.raspberryjammod.api.APIHandler;
+import mobi.omegacentauri.raspberryjammod.network.NetworkHandler;
+import mobi.omegacentauri.raspberryjammod.network.message.RawErrorMessage;
 import mobi.omegacentauri.raspberryjammod.util.PathUtility;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
@@ -26,70 +28,17 @@ import net.minecraftforge.fml.relauncher.Side;
 public class RJMProcessCode {
 
 	private static String scriptProcessorPath;
+	private static Process runningScript;
 
-	public static void run(List<String> program, EntityPlayer player) {
-		try {
-			String path = PathUtility.getPythonExecutablePath();
-			if (path.contains("/") || path.contains(System.getProperty("file.separator"))) {
-				scriptProcessorPath = new File(path).getAbsolutePath().toString();
-			} else {
-				scriptProcessorPath = path;
-			}
+	private static String lineNum = "";
 
-			List<String> cmd = new ArrayList<String>();
-			cmd.add(scriptProcessorPath);
-			cmd.add("-i");
+	private static String codeLine = "";
 
-			ProcessBuilder builder = new ProcessBuilder(scriptProcessorPath);
-			// builder.redirectErrorStream(true);
-
-			builder.directory(new File("mcpipy/"));
-
-			Map<String, String> environment = builder.environment();
-			environment.put("MINECRAFT_PLAYER_NAME", player.getName());
-			environment.put("MINECRAFT_PLAYER_ID", "" + player.getEntityId());
-			environment.put("MINECRAFT_API_PORT", "" + RaspberryJamMod.currentPortNumber);
-
-			builder.command(cmd);
-
-			Process process = builder.start();
-
-			gobble(process.getInputStream(), player, "");
-			gobble(process.getErrorStream(), player, "[ERR] ");
-
-			OutputStream in = process.getOutputStream();
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(in));
-
-			boolean codeBlock = false;
-			int blockDepth = 0;
-			for (String lines : program) {
-				if (codeBlock && lines.split(Pattern.quote("    ")).length - 1 < blockDepth) {
-					for (int i = blockDepth - lines.split(Pattern.quote("    ")).length - 1; i > 0; i--) {
-						writer.newLine();
-					}
-					blockDepth = lines.split(Pattern.quote("    ")).length - 1;
-					if (blockDepth == 0) {
-						codeBlock = false;
-					}
-				}
-				writer.write(lines);
-				if (lines.contains("    ")) {
-					blockDepth = lines.split(Pattern.quote("    ")).length - 1;
-					codeBlock = true;
-				}
-				writer.newLine();
-			}
-			if (codeBlock) {
-				for (int i = blockDepth; i > 0; i--) {
-					writer.newLine();
-				}
-			}
-			writer.write("exit()");
-			writer.newLine();
-			writer.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private static void globalMessage(String msg) {
+		if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
+			Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(msg));
+		} else {
+			APIHandler.globalMessage(msg);
 		}
 	}
 
@@ -129,11 +78,137 @@ public class RJMProcessCode {
 		t.start();
 	}
 
-	private static void globalMessage(String msg) {
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-			Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(msg));
-		} else {
-			APIHandler.globalMessage(msg);
+	private static void gobbleError(final InputStream stream, final EntityPlayer entity, final String label) {
+		Thread t = new Thread() {
+
+			@Override
+			public void run() {
+				BufferedReader br;
+
+				br = new BufferedReader(new InputStreamReader(stream));
+
+				String line;
+				try {
+					while (null != (line = br.readLine())) {
+						if (!line.contains("copyright") && !line.contains("Python")) {
+							if (line.contains(">>>")) {
+								lineNum = line;
+							} else {
+								// stop the script from executing
+								if (line.contains("Error:")) {
+									try {
+										runningScript.exitValue();
+									} catch (IllegalThreadStateException e) {
+										// script was still running
+										runningScript.destroy();
+									}
+									// send the error to the player and then
+									// determine the problem client side
+									NetworkHandler.channel.sendTo(new RawErrorMessage(codeLine, line, lineNum),
+											(EntityPlayerMP) entity);
+									lineNum = "";
+									codeLine = "";
+									// only report the first error
+									break;
+								} else if (!line.contains("<stdin>") && !line.trim().equals("^")) {
+									codeLine = line;
+								}
+							}
+						}
+					}
+				} catch (IOException e) {
+				}
+
+				try {
+					br.close();
+				} catch (IOException e) {
+				}
+			}
+		};
+		t.setDaemon(true);
+		t.start();
+	}
+
+	private static boolean isProcessAlive(Process proc) {
+		try {
+			proc.exitValue();
+			return false;
+		} catch (Exception e) {
+			return true;
+		}
+
+	}
+
+	public static void run(List<String> program, EntityPlayer player) {
+		try {
+			String path = PathUtility.getPythonExecutablePath();
+			if (path.contains("/") || path.contains(System.getProperty("file.separator"))) {
+				scriptProcessorPath = new File(path).getAbsolutePath().toString();
+			} else {
+				scriptProcessorPath = path;
+			}
+
+			if ((runningScript != null) && isProcessAlive(runningScript)) {
+				runningScript.destroy();
+			}
+
+			List<String> cmd = new ArrayList<String>();
+			cmd.add(scriptProcessorPath);
+			cmd.add("-i");
+
+			ProcessBuilder builder = new ProcessBuilder(scriptProcessorPath);
+
+			builder.directory(new File("mcpipy/"));
+
+			Map<String, String> environment = builder.environment();
+			environment.put("MINECRAFT_PLAYER_NAME", player.getName());
+			environment.put("MINECRAFT_PLAYER_ID", "" + player.getEntityId());
+			environment.put("MINECRAFT_API_PORT", "" + RaspberryJamMod.currentPortNumber);
+
+			builder.command(cmd);
+
+			runningScript = builder.start();
+
+			gobble(runningScript.getInputStream(), player, "");
+			gobbleError(runningScript.getErrorStream(), player, "[ERR] ");
+
+			OutputStream in = runningScript.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(in));
+
+			boolean codeBlock = false;
+			int blockDepth = 0;
+			for (String lines : program) {
+				// if (!isProcessAlive(runningScript)) {
+				// writer.flush();
+				// break;
+				// }
+				if (codeBlock && ((lines.split(Pattern.quote("    ")).length - 1) < blockDepth)) {
+					for (int i = blockDepth - lines.split(Pattern.quote("    ")).length - 1; i > 0; i--) {
+						writer.newLine();
+					}
+					blockDepth = lines.split(Pattern.quote("    ")).length - 1;
+					if (blockDepth == 0) {
+						codeBlock = false;
+					}
+				}
+				writer.write(lines);
+				if (lines.contains("    ")) {
+					blockDepth = lines.split(Pattern.quote("    ")).length - 1;
+					codeBlock = true;
+				}
+				writer.newLine();
+			}
+			if (codeBlock) {
+				for (int i = blockDepth; i > 0; i--) {
+					writer.newLine();
+				}
+			}
+			writer.write("exit()");
+			writer.newLine();
+			writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
