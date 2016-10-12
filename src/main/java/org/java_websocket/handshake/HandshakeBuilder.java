7224833a7fc35486f6548336d9b94e19906f4964
Dominic Amato <dominic.a.amato@gmail.com>
2016-10-12 16:07:33 -0500

    drop websocket source add in gradle compile depenedency

--- src/main/java/org/java_websocket/handshake/HandshakeBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.java_websocket.handshake;
-
-public interface HandshakeBuilder extends Handshakedata {
-	public abstract void put(String name, String value);
-
-	public abstract void setContent(byte[] content);
-}
