7b04be613656f4e67fd4b0cde553c27bed492eb0
DomAmato <dominic.a.amato@gmail.com>
2016-11-14 04:03:15 -0600

    update code event to include player

--- src/main/java/mobi/omegacentauri/raspberryjammod/network/CodeEvent.java
+++ src/main/java/mobi/omegacentauri/raspberryjammod/network/CodeEvent.java
@@ -4,16 +4,22 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 public class CodeEvent extends Event {
-
+	
 	public static class ErrorEvent extends CodeEvent {
+		private final EntityPlayer player;
 		private final String code;
 		private final String error;
 		private final int line;
 
-		public ErrorEvent(String code, String error, int line) {
+		public ErrorEvent(String code, String error, int line, EntityPlayer player) {
 			this.code = code;
 			this.error = error;
 			this.line = line;
+			this.player = player;
+		}
+
+		public EntityPlayer getPlayer() {
+			return player;
 		}
 
 		public String getCode() {
@@ -31,12 +37,18 @@ public class CodeEvent extends Event {
 
 	public static class FailEvent extends CodeEvent {
 
+		private final EntityPlayer player;
 		private final String code;
 		private final int id;
 
-		public FailEvent(String code, int id) {
+		public FailEvent(String code, int id, EntityPlayer player) {
 			this.code = code;
 			this.id = id;
+			this.player = player;
+		}
+
+		public EntityPlayer getPlayer() {
+			return player;
 		}
 
 		public String getCode() {
@@ -49,8 +61,9 @@ public class CodeEvent extends Event {
 	}
 
 	public static class SocketCloseEvent extends CodeEvent {
-		private final EntityPlayer player;
 
+		private final EntityPlayer player;
+		
 		public SocketCloseEvent(EntityPlayer player) {
 			this.player = player;
 		}
@@ -62,12 +75,19 @@ public class CodeEvent extends Event {
 
 	public static class SuccessEvent extends CodeEvent {
 
+		private final EntityPlayer player;
+		
 		private final String code;
 		private final int id;
 
-		public SuccessEvent(String code, int id) {
+		public SuccessEvent(String code, int id, EntityPlayer player) {
 			this.code = code;
 			this.id = id;
+			this.player = player;
+		}
+
+		public EntityPlayer getPlayer() {
+			return player;
 		}
 
 		public String getCode() {
