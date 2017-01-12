250ac1375cf789ec152c60bc2b94f1ffca79e4e4
Dominic Amato <dominic.a.amato@gmail.com>
2017-01-11 21:04:17 -0600

    Robot API improvements

--- mcpipy/robot.py
+++ mcpipy/robot.py
@@ -4,11 +4,10 @@
 
 import mcpi.minecraft as minecraft
 import mcpi.block as block
+import mcpi.facing as facing
 from mcpi.vec3 import Vec3
 from mcpi.util import flatten, floorFlatten
 import time
-
-Block = block.Block
 
 class Robot:
 
@@ -17,6 +16,9 @@ class Robot:
         self.robotId = self.mc.conn.sendReceive("robot.id")
         self.delayTime = 0.1
 
+    def __str__(self):
+        return self.mc.conn.sendReceive("robot.name", self.robotId)
+        
     def robot(self):
         """Initialize the Robot"""
         self.robotId = self.mc.conn.sendReceive("robot.id")
@@ -24,10 +26,13 @@ class Robot:
     def inspect(self, *args):
         """Get block with data (x,y,z) => Block"""
         if len(args) > 0:
-            ans = self.mc.conn.sendReceive_flat("robot.inspect", self.robotId, floorFlatten(args))
+            newArgs = [int(self.robotId)]
+            for arg in args:
+                newArgs.append(arg)
+            ans = self.mc.conn.sendReceive_flat("robot.inspect", floorFlatten(newArgs))
         else:
             ans = self.mc.conn.sendReceive("robot.inspect", self.robotId)
-        return Block(*[int(x) for x in ans.split(",")[:2]])
+        return block.Block(*[int(x) for x in ans.split(",")[:2]])
 
     def turn(self, angle):
         """Compass turn of robot (turn:float/int) in degrees: 0=south, 90=west, 180=north, 270=west"""
@@ -45,24 +50,50 @@ class Robot:
     def right(self):
         """Turn clockwise relative to compass heading"""
         self.turn(90)
+        
+    def setFacing(self, dir):
+        if type(dir) is int:
+            self.mc.conn.send("robot.face", self.robotId, facing.fromId(dir).id)
+        if type(dir) is str:
+            if len(dir) == 1:
+                self.mc.conn.send("robot.face", self.robotId, facing.fromLetter(dir).id)
+            else:
+                self.mc.conn.send("robot.face", self.robotId, facing.fromName(dir).id)
+        self.delay()
 
     def forward(self, distance=1):
         """Move robot forward (distance: float)"""
         self.mc.conn.sendReceive("robot.forward", self.robotId, distance)
 
+    def climb(self, distance=1):
+        """Move robot climb (distance: float)"""
+        self.mc.conn.sendReceive("robot.climb", self.robotId, distance)
+        
+    def descend(self, distance=1):
+        """Move robot climb (distance: float)"""
+        self.mc.conn.sendReceive("robot.descend", self.robotId, distance)
+
     def placeBlock(self, *args):
         """Place block (id,[data]), can be empty so robot uses inventory"""
         if len(args) > 0:
-            self.mc.conn.send_flat("robot.place", self.robotId, floorFlatten(*args))
+            newArgs = [int(self.robotId)]
+            for arg in args:
+                newArgs.append(arg)
+            self.mc.conn.sendReceive_flat("robot.place", floorFlatten(newArgs))
         else:
-            self.mc.conn.send("robot.place", self.robotId)
+            self.mc.conn.sendReceive("robot.place", self.robotId)
+        self.delay()
 
     def breakBlock(self, *args):
         """Breaks block in front of robot, else within 1x1x1 range of robot (x,y,z)"""
         if len(args) > 0:
-            self.mc.conn.send_flat("robot.break", self.robotId, floorFlatten(*args))
+            newArgs = [int(self.robotId)]
+            for arg in args:
+                newArgs.append(arg)
+            self.mc.conn.sendReceive_flat("robot.break", floorFlatten(newArgs))
         else:
-            self.mc.conn.send("robot.break", self.robotId)
+            self.mc.conn.sendReceive("robot.break", self.robotId)
+        self.delay()
 
     def backward(self, distance=1):
         """Move robot backwards, will change heading"""
@@ -70,15 +101,17 @@ class Robot:
         
     def say(self, phrase):
         """Have the robot speak"""
-        self.mc.conn.send("robot.say", self.robotId, phrase)
+        self.mc.conn.send("robot.say", self.robotId, phrase.__str__())
+        self.delay()
         
     def jump(self):
         """Make robot jump"""
-        self.mc.conn.send("robot.jump", self.robotId)
+        self.mc.conn.sendReceive("robot.jump", self.robotId)
         
-    def interact(self):
+    def interact(self): 
         """Have the robot interact with the environment"""
         self.mc.conn.send("robot.interact", self.robotId)
+        self.delay()
     
     def getDirection(self):
         """Get entity direction (entityId:int) => Vec3"""
